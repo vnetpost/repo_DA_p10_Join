9a59b690b42b6f676dded87c155069291a8b8582
Sonja Sander <sonja.sander3@gmail.com>
2026-01-16 13:33:24 +0100

    projectNumber and version deleted

--- join/src/app/app.config.ts
+++ join/src/app/app.config.ts
@@ -10,6 +10,18 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "join-4d381", appId: "1:424862204371:web:fec3613f70b8c42df26f26", storageBucket: "join-4d381.firebasestorage.app", apiKey: "AIzaSyC3a_L4kha0fw5Wm5GxUhDX_9Ak4o6VEVM", authDomain: "join-4d381.firebaseapp.com", messagingSenderId: "424862204371", projectNumber: "424862204371", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
+    provideRouter(routes), 
+    provideFirebaseApp(() => initializeApp(
+      { projectId: "join-4d381", 
+        appId: "1:424862204371:web:fec3613f70b8c42df26f26", 
+        storageBucket: "join-4d381.firebasestorage.app", 
+        apiKey: "AIzaSyC3a_L4kha0fw5Wm5GxUhDX_9Ak4o6VEVM", 
+        authDomain: "join-4d381.firebaseapp.com", 
+        messagingSenderId: "424862204371" 
+        // projectNumber: "424862204371", 
+        // version: "2" 
+      })), 
+      provideAuth(() => getAuth()), 
+      provideFirestore(() => getFirestore())
   ]
 };
