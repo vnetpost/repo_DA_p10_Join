baab902f6a4ac13adc29e24f6f1c7cd1e854eb1d
Sonja Sander <sonja.sander3@gmail.com>
2026-01-21 22:16:01 +0100

    legal pages edited

--- join/src/app/imprint/imprint.ts
+++ join/src/app/imprint/imprint.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-imprint',
@@ -6,6 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './imprint.html',
   styleUrl: './imprint.scss',
 })
-export class Imprint {
+export class Imprint implements OnInit{
+  location = inject(Location);
 
+  ngOnInit(): void {
+    const el = document.querySelector('.content-area');
+    if (el) {
+      el.scrollTop = 0;
+    }
+  }
+
+  back(): void {
+    this.location.back();
+  }
 }
