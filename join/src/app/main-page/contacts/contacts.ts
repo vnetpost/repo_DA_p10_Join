e375713f8eff0e09fbe7679b94da317016a7da0e
Sonja Sander <sonja.sander3@gmail.com>
2026-01-22 09:09:42 +0100

    contacts layout copied from main-page to contacts component

--- join/src/app/main-page/contacts/contacts.ts
+++ join/src/app/main-page/contacts/contacts.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ContactList } from './contact-list/contact-list';
+import { ContactInfo } from './contact-info/contact-info';
+import { ContactDialog } from './contact-dialog/contact-dialog';
 
 @Component({
   selector: 'app-contacts',
-  imports: [],
+  imports: [ContactList, ContactInfo, ContactDialog],
   templateUrl: './contacts.html',
   styleUrl: './contacts.scss',
 })
--- join/src/app/main-page/main-page.ts
+++ join/src/app/main-page/main-page.ts
@@ -1,12 +1,9 @@
 import { Component, inject } from '@angular/core';
-import { ContactList } from './contacts/contact-list/contact-list';
-import { ContactDialog } from './contacts/contact-dialog/contact-dialog';
-import { ContactInfo } from './contacts/contact-info/contact-info';
 import { FirebaseService } from '../shared/services/firebase-service';
 
 @Component({
   selector: 'app-main-page',
-  imports: [ContactList, ContactInfo, ContactDialog],
+  imports: [],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
