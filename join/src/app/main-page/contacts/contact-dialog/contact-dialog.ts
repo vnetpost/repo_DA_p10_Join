a6ad82bdfecaf0f804d2830a614eb5eb20d04f73
Sonja Sander <sonja.sander3@gmail.com>
2026-01-26 11:49:38 +0100

    edited

--- join/src/app/main-page/contacts/contact-dialog/contact-dialog.ts
+++ join/src/app/main-page/contacts/contact-dialog/contact-dialog.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
@@ -8,15 +8,38 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './contact-dialog.scss',
 })
 export class ContactDialog {
-  // showDialog(dialog: HTMLDialogElement): void{
-  //   dialog.showModal();
-  // }
+
+  @ViewChild('contactDialog')
+  dialog!: ElementRef<HTMLDialogElement>;
 
   contactData = {
     name: '',
     email: '',
     phone: '',
   };
+
+  openDialog(): void {
+    const el = this.dialog.nativeElement;
+    el.showModal();
+    el.classList.add('opened');
+  }
+
+  closeDialog(): void {
+    const el = this.dialog.nativeElement;
+    el.classList.remove('opened');
+    el.close();
+  }
+
+  onBackdropClick(event: MouseEvent): void {
+    if (event.target === this.dialog.nativeElement) {
+      this.closeDialog();
+    }
+  }
+
+  onEsc(event: Event): void {
+    event.preventDefault();
+    this.closeDialog();
+  }
 
   onSubmit(form: NgForm): void {
     if (form.invalid) {
@@ -27,8 +50,4 @@ export class ContactDialog {
     console.log('Contact created:', this.contactData);
     form.resetForm();
   }
-
-  // closeDialog(): void {
-    
-  // }
 }
