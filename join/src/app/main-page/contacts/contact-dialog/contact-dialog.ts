3ec17d55cf5dee0d83b2a885496abf547ea0a998
Sonja Sander <sonja.sander3@gmail.com>
2026-01-26 15:12:58 +0100

    dialog modes added

--- join/src/app/main-page/contacts/contact-dialog/contact-dialog.ts
+++ join/src/app/main-page/contacts/contact-dialog/contact-dialog.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { Contact } from '../../../shared/interfaces/contact';
 
 @Component({
   selector: 'app-contact-dialog',
@@ -8,9 +9,11 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './contact-dialog.scss',
 })
 export class ContactDialog {
-
   @ViewChild('contactDialog')
   dialog!: ElementRef<HTMLDialogElement>;
+
+  dialogMode: 'add' | 'edit' = 'add';
+  // dialogMode: 'add' | 'edit' = 'edit';
 
   contactData = {
     name: '',
@@ -18,12 +21,71 @@ export class ContactDialog {
     phone: '',
   };
 
+  @Output() save = new EventEmitter<Contact>();
+  @Output() delete = new EventEmitter<string>();
+
+
+  // #region Methods
+  // #region Opening dialog
+  openAddDialog(): void {
+    this.dialogMode = 'add';
+
+    this.contactData = {
+      name: '',
+      email: '',
+      phone: '',
+    };
+
+    this.openDialog();
+  }
+
+  openEditDialog(contact: Contact): void {
+    this.dialogMode = 'edit';
+
+    this.contactData.name = contact.name;
+    this.contactData.email = contact.email;
+    this.contactData.phone = String(contact.phone);
+
+    this.openDialog();
+  }
+
   openDialog(): void {
     const el = this.dialog.nativeElement;
     el.showModal();
     el.classList.add('opened');
   }
+  // #endregion
 
+  onSubmit(form: NgForm): void {
+    if (form.invalid) {
+      form.control.markAllAsTouched();
+      return;
+    }
+
+    if (this.dialogMode === 'add') {
+      this.createContact();
+    } else {
+      this.updateContact();
+
+    }
+
+    this.closeDialog();
+    form.resetForm({
+      name: '',
+      email: '',
+      phone: '',
+    });
+  }
+
+  createContact(): void {
+    console.log('Created:', this.contactData);
+  }
+
+  updateContact(): void {
+    console.log('Updated:', this.contactData);
+  }
+
+  // #region Closing dialog
   closeDialog(): void {
     const el = this.dialog.nativeElement;
     el.classList.remove('opened');
@@ -40,14 +102,6 @@ export class ContactDialog {
     event.preventDefault();
     this.closeDialog();
   }
-
-  onSubmit(form: NgForm): void {
-    if (form.invalid) {
-      form.control.markAllAsTouched();
-      return;
-    }
-
-    console.log('Contact created:', this.contactData);
-    form.resetForm();
-  }
+  // #endregion
+  // #endregion
 }
