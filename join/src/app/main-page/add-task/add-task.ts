89c9b161094741077c7aeb618e17580a3991f638
vnetpost <vnetpost@gmail.com>
2026-02-02 08:13:54 +0000

    add-task add-task-a add-task-b grid-desiged

--- join/src/app/main-page/add-task/add-task.ts
+++ join/src/app/main-page/add-task/add-task.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { AddTaskA } from './add-task-a/add-task-a';
+import { AddTaskB } from './add-task-b/add-task-b';
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
+  imports: [AddTaskA, AddTaskB],
   templateUrl: './add-task.html',
   styleUrl: './add-task.scss',
 })
-export class AddTask {
-
-}
+export class AddTask {}
