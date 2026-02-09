5a380914d91e11af384339c0b2f3a728fc962065
Sonja Sander <sonja.sander3@gmail.com>
2026-02-09 16:20:44 +0100

    login form validation and aria labels added

--- join/src/app/main-page/main-page.ts
+++ join/src/app/main-page/main-page.ts
@@ -1,16 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { FirebaseService } from '../shared/services/firebase-service';
 import { RouterLink } from "@angular/router";
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-main-page',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
 export class MainPage {
   firebaseService = inject(FirebaseService);
   signUp = false;
+  confirmPassword = '';
   showLogInPassword = false;
   showSignUpPassword = false;
   showConfirmPassword = false;
@@ -33,5 +35,34 @@ export class MainPage {
 
   toggleConfirmPassword(): void {
     this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  logInData: any = {
+    email: '',
+    password: '',
+  };
+
+  signUpData: any = {
+    name: '',
+    email: '',
+    password: '',
+  };
+
+  onLogin(form: NgForm): void {
+    if (form.invalid) {
+      return;
+    }
+
+    console.log('Login:', this.logInData);
+  }
+
+  onSignUp(form: NgForm): void {
+    if (form.invalid || this.signUpData.password !== this.confirmPassword) {
+      return;
+    }
+
+    this.signUpData.password = this.signUpData.password;
+
+    console.log('Sign up:', this.signUpData);
   }
 }
