df70f39e97d9f42c63e150ebe478dae2062c6c72
vnetpost <vnetpost@gmail.com>
2026-02-13 11:27:36 +0000

    firebase-configs are now hier environments/environment.ts

--- join/src/app/app.config.ts
+++ join/src/app/app.config.ts
@@ -8,24 +8,14 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'join-4d381',
-        appId: '1:424862204371:web:fec3613f70b8c42df26f26',
-        storageBucket: 'join-4d381.firebasestorage.app',
-        apiKey: 'AIzaSyC3a_L4kha0fw5Wm5GxUhDX_9Ak4o6VEVM',
-        authDomain: 'join-4d381.firebaseapp.com',
-        messagingSenderId: '424862204371',
-        // projectNumber: "424862204371",
-        // version: "2"
-      }),
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
   ],
